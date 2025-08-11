9eb0af122dc97e6997147558cc74b6b3f3afc1a3
unknown <qsrmg121@gmail.com>
2025-08-10 21:14:06 -0700

    @ghghgh

---
+++ dict.py
@@ -0,0 +1,20 @@
+
+
+
+#dict={"a:1,"b":2,"c":3}
+
+# dict["name":"ali,"age":30,):aliali@gmail.com,study:subject;)
+
+#dict["a"]="sarfarz"
+#print(dict.get("a"))
+#xdict.value()
+#dict.pop("age)
+#print(list(x))
+
+
+
+
+#get the value second highest value
+#current_grade=(2022:"80",2023:"90",2024:"70",2025:"85")
+# x = dict.value()
+#print (list(x))
---
+++ loop.py
@@ -0,0 +1,29 @@
+
+
+#furit=["apple","banana","cheery"]
+#furits[2]
+#print(furit[2])
+
+#for abv in furits:
+#    print(abv)
+
+
+for ali in range( 1,10):
+  
+ print(ali)
+ if ali ==5:
+     print("number is 5")
+else:
+    print ("ali is here")
+
+#furit=["apple","banana","cheery"]   
+# for x in "banana":
+#    print(x)
+#   if x ="banana":
+#   break
+#  print(x)
+   # adj=["red","big","tasty"]
+   # furit=["appale","banana","cherry"]
+   # for xin adj:
+   # for y in furits:
+   # print(x,y)
