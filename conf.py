cb67d5d109f2b3fd05815cfb8e38065ca22a2621
unknown <qsrmg121@gmail.com>
2025-08-11 20:59:10 -0700

    fffff

---
+++ conf.py
@@ -0,0 +1,70 @@
+
+
+
+#a = 5
+#b = 10
+
+#if a == b:
+   # print("a is equal to b")
+   # print(a == b) 
+#elif    a < b:
+    #print("a is less than b") 
+    #print(a < b)
+    #print(a == b)
+#else:
+   # print("a is not equal to b")
+
+#print( a > b )
+#print(a == b)
+
+#a = 2
+#b = 330
+#print("A") if a > b else print("B")
+#print("A") if a > b else print("=") if a == b else print("B")
+
+
+
+#artimatic operator
+
+
+a = 5
+b = None
+c = 15
+
+
+if a or b or c:
+    print("both a and b are non-zero")
+else:
+    print("at least one of a or b is zero")
+
+   # list = [5,10,15,20,25]
+   # if 5 in list1:
+   # print("5 is in the list")
+#else
+   #print("5 is not in list")
+
+
+
+
+
+
+x = 30
+
+#if x  > 10:
+   # print("above ten,")
+   # if x > 20:
+     #   print ("and also above 20!")
+    # elif x == 30:
+      #  print("x is also above 30")
+     #else:
+        #print("but not above 20.")
+
+       
+if x  > 10:
+    print("above ten,")
+    if x > 20:
+        print ("and also above 20!")
+    elif x == 30:
+        print("x is also above 30")
+    else:
+        print("but not above 20.")
